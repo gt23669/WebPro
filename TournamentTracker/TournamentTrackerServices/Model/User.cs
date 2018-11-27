1cb98c02a9a6d166ebebe93e6ebbcc8d57951323
SCrawford81299 <thestevencrawford@gmail.com>
2018-11-26 17:49:30 -0700

    Added Email to User

--- TournamentTracker/TournamentTrackerServices/Model/User.cs
+++ TournamentTracker/TournamentTrackerServices/Model/User.cs
@@ -8,6 +8,7 @@ namespace TournamentTrackerServices.Model
 {
     public class User
     {
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Wins { get; set; }
